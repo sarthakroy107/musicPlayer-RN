091b38059c5b9d1410be18874750c9d34064c64c
sarthakroy107 <sarthakroy107@gmail.com>
2023-08-03 23:08:05 +0530

    Minor UI and image changes

--- src/constants.ts
+++ src/constants.ts
@@ -7,7 +7,7 @@ export const playlistData: Track[] = [
       title: 'Zom 100',
       artist: 'KANA-BOON',
       album: 'Zom 100',
-      artwork: 'https://i.scdn.co/image/ab67706c0000da84586f7174df51e34af322ed1d',
+      artwork: 'https://www.cnet.com/a/img/resize/9f75f61bfb5441d8a2e0eecb94bd48fec3e01e3e/hub/2023/07/07/50bca37c-159c-4055-8f02-291d3bba2df7/zom-100.jpg?auto=webp&width=1200',
       url: require('./assets/audio/one.mp3'),
     },
     {
@@ -16,7 +16,7 @@ export const playlistData: Track[] = [
       artist: 'Enako',
       album: 'Oshi***',
       artwork:
-        'https://i.scdn.co/image/ab67616d0000b273175e30983b43f22a0e3085e9',
+        'https://imgs.search.brave.com/8ik1Bvvre0Yi2v1RlhK1pr1gaJdZxMfeHpXxTPS_F0c/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9wcmV2/aWV3LnJlZGQuaXQv/YS10aGluZy1pLWRp/ZC1hbmQtaS1oYXZl/LWFub3RoZXItbWFo/aXJvLWZhbmFydC1p/bi1hLXJlYWxseS12/MC1zOG1vZWlpN2g2/YWIxLmpwZz93aWR0/aD02NDAmY3JvcD1z/bWFydCZhdXRvPXdl/YnAmcz1iMGEwMTk2/YmQ2Yjg2ZjYzNjU4/YzExNzNjYmJjYzEw/NmI4M2U5ZTEy',
       url: require('./assets/audio/two.mp3'),
     },
     {
@@ -43,7 +43,7 @@ export const playlistData: Track[] = [
       artist: 'Rie Takahashi',
       album: 'The Angel Next Door Spoils Me Rotten',
       artwork:
-        'https://i.scdn.co/image/ab67616d0000b27396191b883d8f235e670829dc',
+        'https://i.scdn.co/image/ab67616d0000b2730ad141380b1ca58e8df28267',
       url: require('./assets/audio/five.mp3'),
     },
     {
@@ -52,7 +52,7 @@ export const playlistData: Track[] = [
         artist: 'MYTH & ROID',
         album: 'Re:Zero',
         artwork:
-          'https://i.scdn.co/image/ab67616d0000b273bb23bfa930d89053e92078ec',
+          'https://i.etsystatic.com/27151483/c/707/707/0/0/il/e54c67/4294060650/il_600x600.4294060650_8gm7.jpg',
         url: require('./assets/audio/six.mp3'),
     },
     {
